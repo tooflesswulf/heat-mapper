564fc585f772c845e5fcd53c0e80f63284fb3948
albertxu <17albertX@students.harker.org>
2017-06-29 10:48:07 -0700

    Spin works headless now. Images are blurry, going to experiment with spin; stop; take pic; spin; stop; take pick...

--- microwave/Pi-side/spin_image.py
+++ microwave/Pi-side/spin_image.py
@@ -4,8 +4,20 @@ from picamera import PiCamera
 import cv2
 import time
 import threading
+import sys
 
 img_store_loc = 'chicken/'
+
+try:
+    msg = sys.argv[1]
+
+except IndexError:
+    show = True
+else:
+    if msg in ['show', '-show', '-s']:
+        show = True
+    else:
+        show = False
 
 def is_ready(s):
     return s.getCurrentPos()[0]==98
@@ -14,29 +26,35 @@ def wait_ready(s):
     while not is_ready(s):
         pass
 
-def save_img(img):
-    timestamp = time.time()
-    cv2.imwrite(img_store_loc + '{}.jpg'.format(timestamp), img)
+def save_img(img, name):
+    cv2.imwrite(name, img)
 
 
-def img_thread(img):
-    if threading.active_count() < 20:
-        thread = threading.Thread(target=save_img, args=[img])
-        thread.start()
+lastsave = 0
+def img_thread(img, save_freq = .5):
+    if threading.active_count() < 20: #Maximum 20 threads
+        global lastsave
+        timestamp = time.time()
+        if timestamp - lastsave > save_freq:
+            lastsave = timestamp
+            save_name = img_store_loc + 'chicken_{}.jpg'.format(timestamp)
+            thread = threading.Thread(target=save_img, args=[img, save_name])
+            thread.start()
     else:
         print('Too many threads! Aborting.')
 
 
 s = stirrer.StirrerControl(port='/dev/ttyUSB0',addr=1)
+wait_ready(s)
 s.controlLights(3)
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (640, 480)
+# camera.resolution = (1280, 960)
 camera.framerate = 32
 camera.vflip = True
 camera.hflip = True
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=camera.resolution)
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -51,14 +69,15 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     img_thread(image)
 
     # show the frame
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
+    if show:
+        cv2.imshow("Frame", image)
+        key = cv2.waitKey(1) & 0xFF
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
 
     # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
+    if show and key == ord("q"):
         break
     if is_ready(s):
         break
